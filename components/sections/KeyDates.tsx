744aac9adda8ec8d271d68811808435334bbdff7
MuhammadOsman381 <mosman257@gmail.com>
2026-08-20 10:43:45 +0300

    Update student return date

--- components/sections/KeyDates.tsx
+++ components/sections/KeyDates.tsx
@@ -5,7 +5,7 @@ import { motion, Reveal, staggerContainer } from '../ui/Motion';
 
 const schoolEvents = [
   { event: 'Staff Return', date: '16 August 2026' },
-  { event: 'Student Return', date: '23 August 2026' },
+  { event: 'Student Return', date: '30 August 2026' },
   { event: 'Term 1 Internal Assessment (Years 3-10)', date: '9-15 December 2026' },
   { event: 'Return from Winter Break (Term 2 Begins)', date: '10 January 2027' },
   { event: 'Parent-Teacher Conference', date: '11 February 2027' },
